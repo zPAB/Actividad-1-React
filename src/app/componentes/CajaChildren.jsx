0f9b768fc16a7b7e1d6873bde9abb10ffcad6eaa
Yesid Alexander Ocampo Hoyos <yesidalexanderocampo@gmail.com>
2025-10-29 07:26:28 -0500

    CajaChildren.jsx

--- src/app/componentes/CajaChildren.jsx
+++ src/app/componentes/CajaChildren.jsx
@@ -1,1 +1,11 @@
+ import React from 'react'
 
+function CajaChildren({ children }) {
+  return (
+    <div className="p-4 border-2 border-blue-400 rounded-lg">
+      {children}
+    </div>
+  )
+}
+
+export default CajaChildren
